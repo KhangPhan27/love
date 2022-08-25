ddf7e28a7bdcb526acc16ece5c8555bb1f7fef52
Khangphan <102360362+KhangPhan27@users.noreply.github.com>
2022-08-25 17:51:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,8 @@
 date_default_timezone_set('Asia/Ho_Chi_Minh');
 $time = date("H:i:s d.m.Y");
 $losi = $_GET['answer'];
-if($losi == '1'){
-    $h = "Co";
-} else {
-    $h = "Khong";
-}
 ///Chỗ này để lưu lại biến GET sang cho dễ làm, chạy web thực nhớ bỏ đi
-$myfile = fopen("index.html", "a") or die("Unable to open file!");
-$txt = $h. ' | ' . $time."\n";
+$myfile = fopen("abc.txt", "a") or die("Unable to open file!");
+$txt = $losi. ' | ' . $time."\n";
 fwrite($myfile, $txt);
 fclose($myfile);
