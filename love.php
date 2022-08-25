6143d264ad3fc32d0e316fbc3b1ed9c86e4ebf08
Khangphan <102360362+KhangPhan27@users.noreply.github.com>
2022-08-25 11:46:10 +0700

    Add files via upload

---
+++ love.php
@@ -0,0 +1,14 @@
+<?php 
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+$time = date("H:i:s d.m.Y");
+$losi = $_GET['answer'];
+if($losi == '1'){
+    $h = "Co";
+} else {
+    $h = "Khong";
+}
+///Chỗ này để lưu lại biến GET sang cho dễ làm, chạy web thực nhớ bỏ đi
+$myfile = fopen("abc.txt", "a") or die("Unable to open file!");
+$txt = $h. ' | ' . $time."\n";
+fwrite($myfile, $txt);
+fclose($myfile);
