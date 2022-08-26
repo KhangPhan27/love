3fe19a851308fef86a35d27aa566cf1702c4f23e
Khangphan <102360362+KhangPhan27@users.noreply.github.com>
2022-08-27 02:56:38 +0700

    Delete love.php

--- love.php
+++
@@ -1,9 +0,0 @@
-<?php 
-date_default_timezone_set('Asia/Ho_Chi_Minh');
-$time = date("H:i:s d.m.Y");
-$losi = $_GET['answer'];
-///Chỗ này để lưu lại biến GET sang cho dễ làm, chạy web thực nhớ bỏ đi
-$myfile = fopen("abc.txt", "a") or die("Unable to open file!");
-$txt = $losi. ' | ' . $time."\n";
-fwrite($myfile, $txt);
-fclose($myfile);
